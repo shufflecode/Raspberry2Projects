2cfa9cc1284097b1b5de10bb2e974e59e1c21cf9
Jakob Gegeniger <jakob.gegeniger@outlook.de>
2016-01-12 21:44:29 +0100

    Hardware dazu ;-)

---
+++ Raspi2Projects/libShared/HardwareNah/AnalogDefinitions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libShared.HardwareNah
+{
+
+    /// <summary>
+    /// Defines the configuration Structure of an analog Channel
+    /// </summary>
+    public struct AnaChConfig
+    {
+        /// <summary>
+        /// Defines the Ratio of the measuring Resistor to the whole Resisor
+        /// </summary>
+        public float ResistorRatio { get; set; }
+        /// <summary>
+        /// Defines the Reference Voltage of the Converter System
+        /// </summary>
+        public float RefVoltage { get; set; }
+        /// <summary>
+        /// Defines the Offset Value for Pseudo-Differential Measerment
+        /// </summary>
+        public float OffsetValue { get; set; }
+
+        // @todo implement HelperClasses for setting Values little bit more idiote proof
+    }
+}
---
+++ Raspi2Projects/libShared/HardwareNah/LEDDefinitions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libShared.HardwareNah
+{
+    /// <summary>
+    /// RGB-LED-Settings for color channels and drive current for one LED
+    /// </summary>
+    public struct RGBValue
+    {
+        /// <summary>
+        /// Defines the Maximum Value that can be set
+        /// </summary>
+        public const int NormValueWidt = 16;
+        public const int MaxValue = UInt16.MaxValue;
+
+        private UInt16 red;
+        /// <summary>
+        /// Red Value
+        /// </summary>
+        public UInt16 Red
+        {
+            get { return red; }
+            set { red = value; }
+        }
+
+        private UInt16 green;
+        /// <summary>
+        /// Green Value
+        /// </summary>
+        public UInt16 Green
+        {
+            get { return green; }
+            set { green = value; }
+        }
+
+        private UInt16 blue;
+        /// <summary>
+        /// Blue Value
+        /// </summary>
+        public UInt16 Blue
+        {
+            get { return blue; }
+            set { blue = value; }
+        }
+
+        private UInt16 intensity;
+        /// <summary>
+        /// Intensity Value
+        /// </summary>
+        public UInt16 Intensity
+        {
+            get { return intensity; }
+            set { intensity = value; }
+        }
+    }
+
+    /// <summary>
+    /// Generic RGB-Color-Definitions
+    /// </summary>
+    public static class RGBDefines
+    {
+        const UInt16 MaxValue = UInt16.MaxValue;
+        public readonly static RGBValue Black = new RGBValue { Intensity = MaxValue, Red = 0, Green = 0, Blue = 0 };
+        public readonly static RGBValue Red = new RGBValue { Intensity = MaxValue, Red = MaxValue, Green = 0, Blue = 0 };
+        public readonly static RGBValue Green = new RGBValue { Intensity = MaxValue, Red = 0, Green = MaxValue, Blue = 0 };
+        public readonly static RGBValue Blue = new RGBValue { Intensity = MaxValue, Red = 0, Green = 0, Blue = MaxValue };
+        public readonly static RGBValue Yellow = new RGBValue { Intensity = MaxValue, Red = MaxValue, Green = MaxValue, Blue = 0 };
+        public readonly static RGBValue Cyan = new RGBValue { Intensity = MaxValue, Red = 0, Green = MaxValue, Blue = MaxValue };
+        public readonly static RGBValue Magenta = new RGBValue { Intensity = MaxValue, Red = MaxValue, Green = 0, Blue = MaxValue };
+        public readonly static RGBValue White = new RGBValue { Intensity = MaxValue, Red = MaxValue, Green = MaxValue, Blue = MaxValue };
+    }
+}
