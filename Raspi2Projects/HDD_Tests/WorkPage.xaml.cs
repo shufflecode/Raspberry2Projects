f915f9f13c7dd29a3d10580331aa9153fe580a7f
Jakob Gegeniger <jakob.gegeniger@outlook.de>
2016-04-05 20:20:46 +0200

    Hall-Drucktaster-Testprojekt

---
+++ Raspi2Projects/HDD_Tests/WorkPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using libShared;
+
+// Die Elementvorlage "Leere Seite" ist unter http://go.microsoft.com/fwlink/?LinkId=234238 dokumentiert.
+
+namespace HDD_Tests
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class WorkPage : Page
+    {
+        public System.Windows.Input.ICommand Command;
+
+        public WorkPage(System.Windows.Input.ICommand command)
+        {
+            this.Command = command;
+            this.InitializeComponent();
+        }
+    }
+}
